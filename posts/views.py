931f42e876680df62273ddd0994b9c77a1f2692d
Carter <csmith@izeni.com>
2016-03-01 19:42:07 -0700

    Going home

--- posts/views.py
+++ posts/views.py
@@ -3,7 +3,7 @@ from posts.serializers import ThreadSerializer, UserSerializer
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status, mixins, generics
+from rest_framework import status, mixins, generics, permissions
 from django.contrib.auth.models import User
 
 class ThreadList(generics.ListCreateAPIView):
@@ -13,9 +13,11 @@ class ThreadList(generics.ListCreateAPIView):
     queryset = Thread.objects.all()
     serializer_class = ThreadSerializer
 
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
-        
+
 class ThreadDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Get/Maniupulate a single thread.
@@ -23,6 +25,8 @@ class ThreadDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Thread.objects.all()
     serializer_class = ThreadSerializer
 
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    
 class UserList(generics.ListAPIView):
     queryser = User.objects.all()
     serializer_class = UserSerializer
